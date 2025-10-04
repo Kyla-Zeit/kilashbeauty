7669cef3e6b4c6c402b41b17c643574e1cd5e471
Kyla <112965597+Kyla-Zeit@users.noreply.github.com>
2025-10-04 15:05:24 +0000

    r

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 // next.config.mjs
-const repo = 'kilashbeauty'
-const isProd = process.env.NODE_ENV === 'production'
+const repo = "kilashbeauty";
+const isProd = process.env.NODE_ENV === "production";
+
 export default {
-  output: 'export',
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
+  output: "export",                // <-- Next puts static site into ./out
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
   images: { unoptimized: true },
   trailingSlash: true,
-}
+};
